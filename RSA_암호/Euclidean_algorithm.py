f11ea074bcada425fa546ef184a70e15f88cc768
okm02 <okm021724@gmail.com>
2026-01-14 16:14:52 +0900

    유클리드 알고리즘_최대공약수 찾기 및 테스트

--- RSA_암호/Euclidean_algorithm.py
+++ RSA_암호/Euclidean_algorithm.py
@@ -8,21 +8,19 @@ def euclidean_algorithm(left, right):
         result_right = result_right % result_left
         if result_right == 0:
             return result_left
+if __name__ == '__main__':
+    print("******두 수의 최대공약수를 찾는 프로그램입니다******")
+    dividend = int(input('첫 번째 수를 입력해주세요\n1️⃣: '))
+    divisor = int(input('두 번째 수를 입력해주세요\n2️⃣: '))
 
-print("******두 수의 최대공약수를 찾는 프로그램입니다******")
-dividend = int(input('첫 번째 수를 입력해주세요\n1️⃣: '))
-divisor = int(input('두 번째 수를 입력해주세요\n2️⃣: '))
+    #편의상 나누는 수를 더 작게 만들었습니다.
+    if dividend < divisor:
+        swap = dividend
+        dividend = divisor
+        divisor = swap
 
-#편의상 나누는 수를 더 작게 만들었습니다.
-if dividend < divisor:
-    swap = dividend
-    dividend = divisor
-    divisor = swap
+    if dividend != divisor:
+        print(f"😎두 수의 최대공약수는 {euclidean_algorithm(dividend, divisor)}입니다.")
 
-if dividend != divisor:
-    print(f"😎두 수의 최대공약수는 {euclidean_algorithm(dividend, divisor)}입니다.")
-
-else:
-    print(f"😎두 수의 최대공약수는 {dividend}입니다.")
-
-
+    else:
+        print(f"😎두 수의 최대공약수는 {dividend}입니다.")
