e4d5e8bc604dd6133551255b24a18fb82be38df8
okm02 <okm021724@gmail.com>
2026-01-16 00:23:59 +0900

    RSA키 생성기

--- RSA_암호/RSA_key.py
+++ RSA_암호/RSA_key.py
@@ -3,7 +3,6 @@ import sys
 #첫 번째 소수와 두 번째 소수, 그리고 RSA_key(d)값은 개인이 보관한다.
 #Euler_n값, 그리고 그와 서로소인 e값은 공개한다.
 #🔒로 공개하면 안되는 수를 표시해두었다.
-
 prime_n1 = int(input("🔒첫 번째 소수 입력\n1️⃣: "))
 for i in range(2, prime_n1):
     if ea.euclidean_algorithm(prime_n1, i) != 1:
@@ -25,9 +24,13 @@ for i in range(2, Euler_n):
     if ea.euclidean_algorithm(Euler_n, i) == 1:
         recommend_number.append(i)
 
+    #메모리 절약
+    if len(recommend_number) > 10:
+        break
+
 #서로소 입력 받기(e)
 Euler_e = int(input(f"{Euler_n} 값과 서로소인 수 입력"
-                    f"\n😊추천: {recommend_number[1]}, {recommend_number[2]}, {recommend_number[3]}..."
+                    f"\n😊추천: {recommend_number[1]}, {recommend_number[3]}, {recommend_number[5]}..."
                     f"\n🗝️: "))
 
 if ea.euclidean_algorithm(Euler_n, Euler_e) != 1:
@@ -35,5 +38,5 @@ if ea.euclidean_algorithm(Euler_n, Euler_e) != 1:
     sys.exit()
 #RSA값 생성 완료
 RSA_key = ea.euclidean_algorithm_2(Euler_n, Euler_e) % Euler_n
-print(f"당신의 RSA_KEY는...\n🔒: {RSA_key}입니다. 타인에게 알리지 마세요.")
+print(f"당신의 RSA_KEY는...\n🔒 {RSA_key}입니다. 타인에게 알리지 마세요.")
 ###################################################################################################################
