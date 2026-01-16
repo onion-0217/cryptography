24d59ad98dd813f3e716af532b3cc783f43cc25a
okm02 <okm021724@gmail.com>
2026-01-16 11:32:11 +0900

    mapping_list

---
+++ RSA_암호/mapping_list.py
@@ -0,0 +1,17 @@
+char_to_num = {}
+
+# 소문자 (00 ~ 25)
+for i in range(26):
+    char = chr(ord('a') + i)
+    char_to_num[char] = f"{i:02d}"
+
+# 대문자 (30 ~ 55)
+for i in range(26):
+    char = chr(ord('A') + i)
+    char_to_num[char] = str(i + 30)
+
+#26~29 사이 중 마음에 드는 수
+char_to_num[' '] = '28'
+
+#숫자 -> 문자 표
+num_to_char = {v: k for k, v in char_to_num.items()}
