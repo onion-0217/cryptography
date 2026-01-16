b5f988633d097a13093f3ae74ba2b4de64dcbb1c
okm02 <okm021724@gmail.com>
2026-01-16 11:31:08 +0900

    RSA키 값을 기반으로 한 암호화 시스템

---
+++ RSA_암호/RSA_Encryption.py
@@ -0,0 +1,51 @@
+import mapping_list
+import Euclidean_algorithm
+from RSA_암호.mapping_list import char_to_num
+from RSA_암호.mapping_list import num_to_char
+
+UNIT = 1
+char_to_num_last = char_to_num['Z']
+#단위를 맞추기 위한 의미없는 값
+padding_char = char_to_num[' ']
+
+sentence_origin = input("🤖 문장을 입력해주세요, 문장부호는 사용을 금합니다.\n")
+n = int(input("🤖 암호화하기 위한 n값과 e값을 입력해주세요\nn: "))
+e = int(input("e: "))
+
+sentence_char_to_num = []
+#문자 -> 숫자
+for i in range(0, len(sentence_origin)):
+    sentence_char_to_num.append(char_to_num[sentence_origin[i]])
+
+#n의 크기에 따른 보안성 변화
+if n < int(f'{char_to_num_last * 2}'):
+    cryp_unit_size = UNIT
+elif n < int(f'{char_to_num_last * 3}'):
+    cryp_unit_size = 2 * UNIT
+elif n < int(f'{char_to_num_last * 4}'):
+    cryp_unit_size = 3 * UNIT
+elif n < int(f'{char_to_num_last * 5}'):
+    cryp_unit_size = 4 * UNIT
+else:
+    cryp_unit_size = 5 * UNIT
+
+#숫자를 단위묶음으로 묶기
+cryp_block = []
+for i in range(0, len(sentence_char_to_num), cryp_unit_size):
+    #파이썬은 인덱싱 에러발생에 대해 관대함을 확인
+    chunk = sentence_char_to_num[i : i + cryp_unit_size]
+
+    if len(chunk) < cryp_unit_size:
+        for i in range(0, cryp_unit_size - len(chunk)):
+            chunk.append(padding_char)
+
+    chunk = "".join(chunk)
+    cryp_block.append(chunk)
+
+#단위묶음을 암호화하는 작업
+for i in range(0, len(cryp_block)):
+    calcul_var = cryp_block[i]
+    cryp_block[i] = str(pow(int(calcul_var), e, n))
+
+cryp_block = "".join(cryp_block)
+print(f'🤖문장 암호의 결과입니다.\nRESULT: {cryp_block}')
